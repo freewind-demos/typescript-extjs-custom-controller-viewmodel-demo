a6e590755fe58f5acedcf11c76b7fd8c22fb849f
Freewind <nowindlee@gmail.com>
2023-04-21 22:02:15 +0800

    fixed

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 TypeScript ExtJS Custom Controller ViewModel Demo
 =================================================
 
-View中的listeners中的events对应的handler，需要注意：
-1. 如果是View中自己的listeners，会找View上定义的method
-2. 如果是View中嵌入的View的listeners，会找View中controller中定义的method，再找View上定义的method
+要使用`viewModel.set('propertyName', 'value')`
 
 ```
 npm install
--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,30 @@
 Ext.onReady(() => {
     new Ext.app.Application({
         name: 'hello',
-        mainView: new Ext.panel.Panel({
+        mainView: new Ext.button.Button({
             renderTo: 'main',
             height: 100,
             width: 200,
-            title: 'Hello world',
             bind: {
-                html: 'Hello, {username}'
+                text: 'Hello, {buttonText}'
             },
             viewModel: new Ext.app.ViewModel({
                 data: {
-                    username: 'username from viewModel'
+                    buttonText: 'button'
                 }
             }),
             controller: new Ext.app.ViewController({
-                onClickUsername: () => {
-                    alert('### controller.onClickUsername');
+                changeText: function () {
+                    const viewModel = this.getViewModel()
+                    viewModel.set('buttonText', 'new button');
                 }
             }),
-            fnOfTheView: () => {
-                alert('### fnOfTheView')
-            },
             listeners: {
-                click: {
-                    element: 'el',
-                    // NOTE view中的fn指向的是view自己定义的method
-                    fn: 'fnOfTheView'
-                }
+                click: function () {
+                    const controller = this.getController()
+                    controller.changeText();
+                },
             },
-            items: [
-                new Ext.button.Button({
-                    text: 'button',
-                    listeners: {
-                        // NOTE 嵌入的'fn'会先指向controller中的method，然后是view自己的method
-                        click: 'onClickUsername',
-                    }
-                })
-            ]
         }),
         launch: () => {
             console.log("### launch")
